c67fe27f604ba37798a0003666b01efadab6cfb3
emiliosantosl <huofnw843@outlook.com>
2025-11-01 12:05:07 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,16 +2,33 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy, execute, read } = deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const goldDeployment = await deploy("ERC7984Gold", {
+    contract: "ERC7984Gold",
+    args: [deployer],
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const minerDeployment = await deploy("Miner", {
+    contract: "Miner",
+    args: [goldDeployment.address],
+    from: deployer,
+    log: true,
+  });
+
+  const currentMinter: string = await read("ERC7984Gold", "minter");
+  if (currentMinter.toLowerCase() !== minerDeployment.address.toLowerCase()) {
+    await execute("ERC7984Gold", { from: deployer, log: true }, "setMinter", minerDeployment.address);
+  }
+
+  console.log(`ERC7984Gold deployed at: ${goldDeployment.address}`);
+  console.log(`Miner deployed at: ${minerDeployment.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_miner_game";
+func.tags = ["Miner"];
